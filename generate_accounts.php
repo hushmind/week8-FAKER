bf95efde64641b7e1f6009df954334773b45f3fd
Correa <161830862+hushmind@users.noreply.github.com>
2025-03-11 12:07:28 +0800

    Bootstrap Updated

--- generate_accounts.php
+++ generate_accounts.php
@@ -3,8 +3,13 @@ require_once 'vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 
-echo "<table border='1'>";
+echo "<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' rel='stylesheet'>";
+echo "<div class='container mt-5'>";
+echo "<table class='table table-bordered'>";
+echo "<thead style='background-color: #f8d7da;'>";
 echo "<tr><th>User ID</th><th>Full Name</th><th>Email Address</th><th>Username</th><th>Password</th><th>Account Created</th></tr>";
+echo "</thead>";
+echo "<tbody>";
 
 for ($i = 0; $i < 10; $i++) {
     $password = $faker->password;
@@ -20,5 +25,7 @@ for ($i = 0; $i < 10; $i++) {
     echo "</tr>";
 }
 
+echo "</tbody>";
 echo "</table>";
+echo "</div>";
 ?>
