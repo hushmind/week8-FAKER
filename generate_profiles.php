887b300090dafb978a1f94c55c37dd8b55c303bd
Correa <161830862+hushmind@users.noreply.github.com>
2025-03-11 11:26:48 +0800

    Philippine Format for Phone Numbers

--- generate_profiles.php
+++ generate_profiles.php
@@ -7,10 +7,14 @@ echo "<table border='1'>";
 echo "<tr><th>Full Name</th><th>Email Address</th><th>Phone Number</th><th>Complete Address</th><th>Birthdate</th><th>Job Title</th></tr>";
 
 for ($i = 0; $i < 5; $i++) {
+    $surname = $faker->lastName;
+    $firstName = $faker->firstName;
+    $fullName = $surname . ', ' . $firstName;
+
     echo "<tr>";
-    echo "<td>" . $faker->name . "</td>";
+    echo "<td>" . $fullName . "</td>";
     echo "<td>" . $faker->email . "</td>";
-    echo "<td>" . $faker->phoneNumber . "</td>";
+    echo "<td>" . $faker->numerify('+63 9## ### ####') . "</td>"; // Custom format for Philippine phone numbers
     echo "<td>" . $faker->address . "</td>";
     echo "<td>" . $faker->date($format = 'Y-m-d', $max = 'now') . "</td>";
     echo "<td>" . $faker->jobTitle . "</td>";
